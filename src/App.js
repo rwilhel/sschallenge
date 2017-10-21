a5fb38d00ca3d470389c39bf0c691e471ba7bc34
rwilhel <rwilhel@gmail.com>
2017-10-21 07:57:45 -0600

    Implements app header and prompt

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">BITCOIN TRANSACTIONS</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and shave to reload.
+          Enter your Bitcoin address to see recent transactions!
         </p>
       </div>
     );
