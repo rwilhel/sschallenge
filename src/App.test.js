27e69109f8c1ac7b4c81cd98c23bb55267c3eb7a
rwilhel <rwilhel@gmail.com>
2017-10-22 19:55:26 -0600

    Passes Jest testing for Search component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from './App';
+import App, { Search } from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -12,6 +12,21 @@ describe('App', () => {
   test('snapshots', () => {
     const component = renderer.create(
       <App />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Search', () => {
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
