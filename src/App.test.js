f44b24624974ae7c197c25da3b0626b3f16658d3
rwilhel <rwilhel@gmail.com>
2017-10-22 19:57:49 -0600

    Passes Jest snapshot testing for Button component

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search } from './App';
+import App, { Search, Button } from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -27,6 +27,21 @@ describe('Search', () => {
   test('snapshots', () => {
     const component = renderer.create(
       <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Delete</Button>, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Button>Delete</Button>
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
